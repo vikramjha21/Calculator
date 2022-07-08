072a9d7e06ac56a89d0f8e1296153b54f311a008
vikramjha21 <vkumar210795@gmail.com>
2022-07-08 17:04:56 +0530

    Created Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator{
+      public static  int add(int a,int b){return a+b;}
+      public static  int sub(int a,int b){return a-b;}
+
+     public static void main(String args[]){
+     
+      System.out.println(add(10,20));
+      System.out.println(sub(20,10));    
+   }
+}
